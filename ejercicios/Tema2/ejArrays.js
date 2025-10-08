23d8bce63a146bf9575260ba6830e281838a4212
jpalorubio <jpalor03@suarezdefigueroa.es>
2025-10-08 22:02:59 +0200

    ej6

--- ejercicios/Tema2/ejArrays.js
+++ ejercicios/Tema2/ejArrays.js
@@ -78,21 +78,41 @@
 //     momento en el que dejarán de pedirse más nombres y se mostrará en pantalla la lista de los
 //     nombres que se han introducido ordenados alfabéticamente.
 
-let bandera = false;
-let miArray = [];
+// let bandera = false;
+// let miArray = [];
 
-while (!bandera) {
-  let nombre = prompt("Introduce un nombre");
-  if (nombre == "") {
-    bandera = true;
-  } else {
-    miArray.push(nombre);
+// while (!bandera) {
+//   let nombre = prompt("Introduce un nombre");
+//   if (nombre == "") {
+//     bandera = true;
+//   } else {
+//     miArray.push(nombre);
+//   }
+// }
+// let rdo = "Lista de nombre: \n";
+// miArray.sort();
+// for (i = 0; i < miArray.length; i++) {
+//   rdo += miArray[i] + "\n";
+// }
+
+// alert(rdo);
+
+// Escribe un script para generar un array de dos dimensiones (matriz). Se le pedirá al usuario
+// primero el tamaño de la matriz (que será cuadrada):
+function crearTabla() {
+  let tamaño = parseInt(prompt("Introduce el tamaño de la matriz cuadrada"));
+  let rdo = "<table border=1>";
+  for (let filas = 1; filas <= tamaño; filas++) {
+    rdo += "<tr>";
+    for (let columnas = 1; columnas <= tamaño; columnas++) {
+      let valor = prompt(
+        "Introduce el valor para la fila " + filas + " y la columna " + columnas
+      );
+      rdo += "<td>&nbsp;&nbsp;" + valor + "&nbsp;&nbsp;</td>";
+    }
+    rdo += "</tr>";
   }
-}
-let rdo = "Lista de nombre: \n";
-miArray.sort();
-for (i = 0; i < miArray.length; i++) {
-  rdo += miArray[i] + "\n";
-}
+  rdo += "</table>";
 
-alert(rdo);
+  document.getElementById("tabla").innerHTML = rdo;
+}
